f455d161fbcb2c7fc10d7e6e34f16bf62857c6ea
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2022-07-02 17:34:48 +0530

    Added solution - LeetHub

---
+++ Clone Graph - GFG/clone-graph.cpp
@@ -0,0 +1,151 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <sstream>
+using namespace std;
+
+struct Node {
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+vector<Node*> bfs(Node *src){
+    vector<Node*>ans;
+    map<Node*, bool> visit;
+    queue<Node*> q;
+    q.push(src);
+    visit[src] = true;
+    while (!q.empty()) {
+        Node *u = q.front();
+        ans.push_back(u);
+        q.pop();
+        vector<Node *> v = u->neighbors;
+        int n = v.size();
+        for (int i = 0; i < n; i++){
+            if (!visit[v[i]]){
+                visit[v[i]] = true;
+                q.push(v[i]);
+            }
+        }
+    }
+    return ans;
+}
+bool checkedClone(Node* prev, Node* new1 ) {
+    vector<Node*>prevAns = bfs(prev);
+    vector<Node*>newAns = bfs(new1);
+    for (int i = 0; i < prevAns.size(); i++) {
+        // cout << prevAns[i] << " " << newAns[i] << endl;
+        if (prevAns[i] == newAns[i]) {
+            // cout << "hii";
+            return false;
+        }
+    }
+    return true;
+}
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// struct Node {
+//     int val;
+//     vector<Node*> neighbors;
+//     Node() {
+//         val = 0;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val) {
+//         val = _val;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val, vector<Node*> _neighbors) {
+//         val = _val;
+//         neighbors = _neighbors;
+//     }
+// };
+
+class Solution {
+public:
+    void dfs(Node *node,unordered_map<int,Node *> &mp)
+    {
+        for(auto it:node->neighbors)
+        {
+            if(mp.find(it->val)==mp.end())
+            {
+                mp[it->val]=it;
+                dfs(it,mp);
+            }
+        }
+        return ;
+    }
+    void merge(Node *node,unordered_map<Node *,Node *> &mpp,unordered_set<Node *> &us)
+    {
+        us.insert(node);
+        for(auto it:node->neighbors)
+        {
+            mpp[node]->neighbors.push_back(mpp[it]);
+            if(us.find(it)==us.end())
+            merge(it,mpp,us);
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        if(node==NULL) return NULL;
+        unordered_map<int,Node *> mp;
+        mp[node->val]=node;
+        dfs(node,mp);
+        
+        unordered_map<Node *,Node *> mpp;
+        for(auto it:mp)
+        {
+            mpp[it.second]=new Node(it.first);
+        }
+        unordered_set<Node *> us;
+        merge(node,mpp,us);
+        return mpp[mp[1]];
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Node* root = NULL;
+        vector<Node*>v(N);
+        std::string buffer;
+        std::getline(std::cin, buffer);
+        for (int i = 0; i < N; i++)v[i] = new Node(i);
+        for (int i = 0; i < N; i++) {
+            std::vector<Node*> vec;
+            std::string buffer;
+            int data;
+            std::getline(std::cin, buffer);
+            std::istringstream iss(buffer);
+            while (iss >> data)
+                vec.push_back(v[data]);
+            v[i]->neighbors = vec;
+        }
+        Solution ob;
+        vector<Node*>prev = bfs(v[0]);
+        Node* ans = ob.cloneGraph(v[0]);
+        vector<Node*>now = bfs(ans);
+        cout << checkedClone(v[0], ans) << endl;
+
+    }
+    return 0;
+}  // } Driver Code Ends
