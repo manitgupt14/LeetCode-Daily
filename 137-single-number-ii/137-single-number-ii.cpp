fb18a6bcfabc86c0e24f6586621b2b8e55e69530
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2022-08-11 11:09:02 +0530

    Time: 11 ms (71.74%), Space: 9.4 MB (98.90%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& arr) {
-        int tmp[33]={0};
-        int N=arr.size();
-        for(int i=0;i<N;i++)
-        {
-            if(arr[i]<0)
-            tmp[32]++;
-            long val=abs(arr[i]);
-            for(int j=0;j<32;j++)
-            { 
-                if(val & 1)
-                    tmp[j]++;
-                val/=2;
+        int ans = 0, k = 3;
+        for(int i=0;i<32;i++){
+            
+            int count = 0;
+            int mask = (1<<i);
+            for(auto n:arr){
+                if((n&mask)!=0) count++;
+            }
+            if(count%k!=0){
+                ans |= (1<<i);
             }
         }
-        
-        long ans=0;
-        for(long i=0;i<32;i++)
-        {
-            if(tmp[i]%3!=0)
-           {
-            ans+=(1<<i);
-           }
-        }
-        
-        if(ans==INT_MAX and tmp[32]%3!=0)
-            return INT_MIN;
-        
-        if(tmp[32]%3!=0)
-        ans*=-1;
-        
         return ans;
     }
 };
