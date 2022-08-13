2baf8aa513f50790756118138721ced572433f8e
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2022-08-13 16:36:56 +0530

    Time: 803 ms (5.00%), Space: 144.3 MB (7.60%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,33 +1,46 @@
+
 class Solution {
 public:
-    static bool cmp(vector<int> &a,vector<int> &b)
+    static bool cmp(vector<int> a,vector<int> b)
     {
         if(a[0]==b[0])
             return a[1]<b[1];
         return a[0]<b[0];
     }
-    int countdigit(int n)
+    int calval(unordered_map<int,int> &mp,int i)
     {
-        int ct=0;
-        while(n>1)
-        {
-            if(n%2==0)
-                n=n/2;
-            else
-                n=3*n+1;
-            ct++;
-        }
-        return ct;
+        if(mp.find(i)!=mp.end())
+            return mp[i];
+        if(i%2!=0)
+        mp[i]=calval(mp,3*i+1)+1;
+        else
+            mp[i]=calval(mp,i/2)+1;
+        return mp[i];
     }
     int getKth(int lo, int hi, int k) {
+        unordered_map<int,int> mp;
+        mp[0]=0;
+        mp[1]=0;
+        mp[2]=1;
+        for(int i=4;i<1002;i*=2)
+        {
+            mp[i]=mp[i/2]+1;
+        }
+        
+        for(int j=1002;j>=3;j--)
+        {
+            if(mp.find(j)==mp.end())
+            {
+                mp[j]=calval(mp,j);
+            }
+        }
+        
         vector<vector<int>> vp;
         for(int i=lo;i<=hi;i++)
         {
-            int ct=countdigit(i);
-            vp.push_back({ct,i});
+            vp.push_back({mp[i],i});
         }
         sort(vp.begin(),vp.end(),cmp);
         return vp[k-1][1];
-        
     }
 };
